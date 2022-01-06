a7de7ecc8d750f43249ed280292c803531181f39
Clemens Solar <solar.c@gmx.net>
2022-01-06 22:22:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# meteoblue-simulation-collector
+
+This repository collects simulated daily data (with hourly resolution) from Meteoblue.
+
+The data can be found in the `./data` directory and contains the data in daily files.
---
+++ bin/collect.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+'use strict';
+const {access, mkdir, writeFile} = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+const Meteoblue = require('../');
+
+
+if (!process.argv[2]) {
+  throw new Error('argument for location path is needed');
+}
+
+(async () => {
+  const meteoblue = new Meteoblue();
+
+  const result = await meteoblue.get(process.argv[2]);
+  const lines = result.toString().split('\n');
+  const grouped = {};
+
+  // Extract location name from first line
+  const [, location] = lines[0].split(',');
+
+  // Data starts at 11th line
+  for (let i = 10; i < lines.length; ++i) {
+    let [date, value] = lines[i].split(',');
+    let time;
+    let day;
+
+    [day, time] = date.split('T');
+
+    value = parseFloat(value);
+
+    day = [
+      day.substring(0, 4),
+      day.substring(4, 6),
+      day.substring(6, 8),
+    ].join('-');
+
+    const dateObj = new Date(
+        `${day}T${time.substring(0, 2)}:${time.substring(2, 4)}:00.000Z`,
+    );
+
+    grouped[day] = grouped[day] || [];
+    grouped[day].push({
+      value,
+      date: dateObj.toISOString(),
+    });
+  }
+
+  // Create directory if not exists
+  const targetDir = path.join(__dirname, `../data/${location.toLowerCase()}`);
+  try {
+    await access(targetDir, fs.constants.F_OK);
+  } catch (err) {
+    await mkdir(targetDir, {recursive: true});
+  }
+
+  const days = Object.keys(grouped);
+  for (const day of days) {
+    const file = `${day}.json`;
+    const targetFile = path.join(targetDir, file);
+
+    console.log(`Write to ${targetFile}`);
+
+    await writeFile(targetFile, JSON.stringify(grouped[day], null, 2));
+  }
+
+  console.log(`Write data for ${days[days.length - 1]}`);
+})();
